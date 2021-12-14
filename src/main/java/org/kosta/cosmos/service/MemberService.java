d8e66574e40a39f2ebe5a7f54cbb9c609004e714
cheoljin408 <kcj409@naver.com>
2021-12-15 02:25:32 +0900

    타임리프 프라그먼트 테스트

--- src/main/java/org/kosta/cosmos/service/MemberService.java
+++ src/main/java/org/kosta/cosmos/service/MemberService.java
@@ -1,12 +1,8 @@
 package org.kosta.cosmos.service;
 
-import org.kosta.cosmos.domain.MemberDTO;
 import org.kosta.cosmos.mapper.MemberMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.AutoConfigureOrder;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 public class MemberService {
